7a65a375109712b2df78870ab04ae5e5b0aa4342
anthony.albertorio <anthony.albertorio@gmail.com>
2019-09-14 17:30:52 -0400

    add lesson5 on creating reducers. first pass is just set up.

---
+++ lesson5.js
@@ -0,0 +1,13 @@
+// Redux: Writing a Counter Reducer with Tests
+
+// reducer for counter
+function counter(state, action) {
+  return state;
+}
+
+module.exports = counter;
+
+/*
+remember:
+reducer = PrevState + CurrentAction -> New State
+*/
