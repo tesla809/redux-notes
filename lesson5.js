0fe787de1648816d0a75901d3a4b2104e37c20c0
anthony.albertorio <anthony.albertorio@gmail.com>
2019-09-14 17:55:27 -0400

    added reducer code and notes

--- lesson5.js
+++ lesson5.js
@@ -1,8 +1,21 @@
 // Redux: Writing a Counter Reducer with Tests
 
 // reducer for counter
+// handles unknown actions by returning current state
+// handles undefined state by returning default state
 function counter(state, action) {
-  return state;
+  // if undefined, must return initial state of app
+  if (typeof state === 'undefined') {
+    return 0;
+  }
+
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return state; // action.type not +/-, return current state
+  }
 }
 
 module.exports = counter;
@@ -10,4 +23,21 @@ module.exports = counter;
 /*
 remember:
 reducer = PrevState + CurrentAction -> New State
+
+state = object that is previous data/state
+action = text that describes action
+strings are good because they are serializable.
+
+is the process of translating data structures 
+or object state into a format that can be stored 
+(for example, in a file or memory buffer) 
+or transmitted (for example, across a network connection link)
+ and reconstructed later 
+ (possibly in a different computer environment)
+
+See: https://en.wikipedia.org/wiki/Serialization
+
+Reducer is in control of application state.
+However, it does not specify the initial state.
+
 */
