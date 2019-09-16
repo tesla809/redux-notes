c5b2a6412796404df8a09b4c06b8c77a6fe4c1af
anthony.albertorio <anthony.albertorio@gmail.com>
2019-09-15 21:14:06 -0400

    Add tests to check for sate mutations w/ deepFreeze package

---
+++ lesson9.test.js
@@ -0,0 +1,45 @@
+// imported deep-freeze to make we can't mutate state 
+// DeepFreeze recursively Object.freeze() objects
+// and functions
+
+// Here we learned how to protect ourselves against 
+// mutations using the deep-freeze package
+// in our tests
+const deepFreeze = require('deep-freeze');
+const { 
+  addCounter, 
+  removeCounter,
+  incrementCounter } = require('./lesson9.js');
+
+test('Adds a counter to list', () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  deepFreeze(listBefore);  // prevents state mutations
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+});
+
+test('Removes a counter from list', () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);  // prevents state mutations
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+});
+
+test('Increments counter in list', () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+});
